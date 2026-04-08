74e02dd6fdf8c71071ec6c63e4628890b3646d74
Osniel Gonzalez <osniel1984@gmail.com>
2026-04-08 09:36:30 -0400

    Upgrade to Node24

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
+  // main.test.ts is skipped until github-action-ts-run-api adds node24 support.
+  // Tracking issue: https://github.com/cardinalby/js-github-action-test-utils
+  testPathIgnorePatterns: ['/node_modules/', '__tests__/main.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
